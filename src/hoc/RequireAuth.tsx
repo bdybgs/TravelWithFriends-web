b0d914f57f0b70df6b75c812ed02f38cbe3e2f2e
bdybgs <bondarev.mc.2018@gmail.com>
2024-03-26 01:09:36 +0300

    added require auth and started login page

---
+++ src/hoc/RequireAuth.tsx
@@ -0,0 +1,15 @@
+import {useLocation, Navigate} from "react-router-dom";
+import React, {ReactNode} from "react";
+
+const RequireAuth = ({children} : { children: ReactNode}) => {
+    const location = useLocation();
+    const auth = false;
+
+    if (!auth) {
+        return <Navigate to="/login" />;
+    }
+
+    return children
+}
+
+export {RequireAuth};
